0a994e6b544bc5ce91cfe17c758d871e6ac3321e
Sanad <07.sanad@gmail.com>
2022-10-19 17:34:49 +0100

    Push all files to git

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const api= {
-    key:'585486ed6c421140c34e0a0213bc2ae8',
-    base:'https://api.openweathermap.org/data/2.5/'
+const api= 'a38b9eab07aec10c48c0273b5defd989';
+  
+window.addEventListener('load', address);
 
-}
+function address() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)   
+    };    
+};
+
+function showPosition(position) {
+    lat = position.coords.latitude;
+    long  = position.coords.longitude;
+    console.log(position);
+    getAddress();
+};
+
+function getAddress(query) {
+    const base = `https://api.openweathermap.org/data/2.5/weatherlat=${lat}&lon=${long}&appid=${api}&units=metric`;
+    
+    console.log(base);
+    fetch(base).then((response) => {
+        return response.json();
+      });    
+} 
+
